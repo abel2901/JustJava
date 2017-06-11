10715ab71972d007e46e0cab57a353fce23a5eab
abel2901 <abel2901@gmail.com>
2017-06-11 18:56:58 -0300

    Added text view
    New text view

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -5,13 +5,15 @@
  * package com.example.android.justjava;
  *
  */
+package com.example.android.justjava;
 
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
-package com.example.android.justjava
+
+import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
@@ -29,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        display(2);
+        displayPrice(2*5);
     }
 
     /**
@@ -39,4 +42,13 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
 }
+
+
